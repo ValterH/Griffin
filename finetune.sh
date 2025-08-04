156bbe6aba072a801496934cadddb08bbfd73a30
ValterH <valter.hudovernik@gmail.com>
2025-08-04 12:50:29 -0700

    refactor example script

--- finetune.sh
+++ finetune.sh
@@ -1,8 +1,41 @@
+# RT comparison
 accelerate launch --config_file hconfig.yaml rt_comparison.py \
     datasets/joint-v65 logs/rel-hm user-churn \
-    --loadpath /lfs/local/0/valter/Griffin/checkpoints/commerce-2/FULL/best_checkpoint/model.safetensors \
+    --loadpath checkpoints/single-sft/best_checkpoint/model.safetensors \
     --seed 0 \
-    --savepath results/hm --tasks rel-hm-user-churn --hop 2 --fanout 20 \
-    --maxepoch 50 --patience 15 --eval_per_epoch 2 \
-    --batchsize 256 --lr 3e-4 --wd 2e-4 --num_mp 4 \
-    --use_rev True --use_gate False --fewshotfanout 3 --hiddim 512 
+    --savepath results/hm \
+    --tasks rel-hm-user-churn \
+    --hop 2 \
+    --fanout 20 \
+    --maxepoch 50 \
+    --patience 15 \
+    --eval_per_epoch 2 \
+    --batchsize 256 \
+    --lr 3e-4 \
+    --wd 2e-4 \
+    --num_mp 4 \
+    --use_rev True \
+    --use_gate False \
+    --fewshotfanout 3 \
+    --hiddim 512
+
+# ORIGINAL
+accelerate launch --config_file hconfig.yaml hmaintask_combine.py \
+    datasets/joint-v65 logs/rel-hm user-churn \
+    --loadpath checkpoints/single-sft/best_checkpoint/model.safetensors \
+    --seed 0 \
+    --savepath results/hm \
+    --tasks rel-hm-user-churn \
+    --hop 2 \
+    --fanout 20 \
+    --maxepoch 50 \
+    --patience 15 \
+    --eval_per_epoch 2 \
+    --batchsize 256 \
+    --lr 3e-4 \
+    --wd 2e-4 \
+    --num_mp 4 \
+    --use_rev True \
+    --use_gate False \
+    --fewshotfanout 3 \
+    --hiddim 512
