9b68713467fada8fa4d1bb44eb4ce62ec345fd47
ValterH <valter.hudovernik@gmail.com>
2025-08-12 14:45:47 -0700

    update conversion script

--- convert.sh
+++ convert.sh
@@ -3,10 +3,10 @@
 DATASET=$1
 
 SRCPATH="datasets/$DATASET-r2n-griffin"
-DSTPATH="datasets/relbench/$DATASET"
+DSTPATH="datasets/relbench-728/$DATASET"
 
 python dataconverter.py $SRCPATH $DSTPATH
-python dataconverteredge.py $SRCPATH $DSTPATH
+python dataconverteredge.py $SRCPATH $DSTPATH --model-dim=728
 python dataconvertertask.py $SRCPATH $DSTPATH
-python dataconverterpost.py $SRCPATH
+python dataconverterpost.py $SRCPATH --model-dim=728
 # python merge_relbench_dataset.py --dataset_name $DATASET --dst_path "datasets/joint-v65"
