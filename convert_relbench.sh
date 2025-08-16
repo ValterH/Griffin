3c75b9c4a8559f30a342dd4c9e953aa1a3b300c1
ValterH <valter.hudovernik@gmail.com>
2025-08-15 19:23:51 -0700

    update conversion script

--- convert_relbench.sh
+++ convert_relbench.sh
@@ -2,11 +2,12 @@
 
 DATASET=$1
 
-python convert_relbench_to_dbinfer_fulltask.py --dataset=$DATASET --output_dir=datasets
+# requires relbench
+python convert_relbench_to_dbinfer_fulltask.py --dataset=$DATASET --output_dir=datasets/728
 
-python -m tab2graph.main preprocess "datasets/$DATASET" transform datasets/$DATASET-raw -c configs/transform/raw_for_griffin.yaml
+python -m tab2graph.main preprocess datasets/728/$DATASET transform datasets/728/$DATASET-raw -c configs/transform/raw_for_griffin.yaml
 
-python -m tab2graph.main preprocess datasets/$DATASET-raw transform datasets/$DATASET-single-griffin -c configs/transform/generate_griffin_feature_separate_num.yaml
+python -m tab2graph.main preprocess datasets/728/$DATASET-raw transform datasets/728/$DATASET-single-griffin -c configs/transform/generate_griffin_feature_separate_num.yaml
 
-python -m tab2graph.main construct-graph datasets/$DATASET-single-griffin r2n-griffin datasets/$DATASET-r2n-griffin
+python -m tab2graph.main construct-graph datasets/728/$DATASET-single-griffin r2n-griffin datasets/728/$DATASET-r2n-griffin
 
